39acb35d4c9742e78845cdb7a5f05f73c39e12ae
fr0stfree <me@frostfree.ru>
2023-04-12 16:26:04 +0300

    review fixes

--- core/errors.js
+++ core/errors.js
@@ -1,8 +1,4 @@
 class ObjectDoesNotExist extends Error {
-  constructor(message, model) {
-    super(message);
-    this.model = model;
-  }
 }
 
 module.exports = ObjectDoesNotExist;
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,6 @@ const router = express.Router();
 
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
-
-router.use((req, res) => {
-  res.status(httpStatus.NOT_FOUND).send({ message: 'Уходи' });
-});
+router.use((req, res) => res.status(httpStatus.NOT_FOUND).send({ message: 'Уходи' }));
 
 module.exports = router;
